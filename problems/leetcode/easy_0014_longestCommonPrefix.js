92976e451b09e5e20b52ef76f0bfd35aba000419
HiJack <hijack.sherlock@live.com>
2022-05-09 22:53:31 +0800

    refactor(leetcode): 14 题添加注释

--- problems/leetcode/easy_0014_longestCommonPrefix.js
+++ problems/leetcode/easy_0014_longestCommonPrefix.js
@@ -11,12 +11,14 @@ if (!strs || strs.length == 0) return ''
   if (strs?.length == 1) return strs[0]
   let common = strs[0]
   for (let i = 1; i < strs.length; i++) {
+    // 若某个字符串为空
     if (strs[i] == '') return ''
     for (let j = 0; j < strs[i].length; j++) {
       if (common[j] != strs[i][j]) {
         common = strs[i].substring(0, j)
         break
       }
+      // 若直到某个字符串的最后一个字符都一致
       if ((j == strs[i].length - 1) && common[j] == strs[i][j]) {
         common = strs[i]
       }
