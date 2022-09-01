4eaa9a0f745eda2321b7a568a8b79a2da0e13ada
HiJack <hijack.sherlock@live.com>
2022-09-01 13:49:10 +0800

    refactor(lecture draft cardsGame): 范围上的尝试模型

--- draft.js
+++ draft.js
@@ -1,46 +1,19 @@
-// 给定两个长度都为 N 的数组 weights 和 values，weights[i] 和 values[i] 分别代表 i 号物品的重量和价值。
+// 给定一个整型数组arr，代表数值不同的纸牌排成一条线，玩家A和玩家B依次拿走每张纸牌，规定玩家A先拿，玩家B后拿，但是每个玩家每次只能拿走最左或最右的纸牌，玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
 
-// 给定一个正数 bag，表示一个载重 bag 的袋子，你装的物品不能超过这个重量。
-
-// 返回你能装下最多的价值是多少?
-
-const knapsack = (weights, values, bag) => {
-	if (!weights || !values || bag <= 0) return 0
-	return process(weights, values, bag, 0, 0, 0)
+const cardsGame = (arr) => {
+	if (!arr || arr.length == 0) return 0
+	return Math.max(first(arr, 0, arr.length - 1), second(arr, 0, arr.length - 1))
 }
 
-// aw: already weight已经装了多重
-const process = (w, v, b, i, aw, av) => {
-	if (aw + w[i] > b || i == w.length) return av
-	
-	let yes = process(w, v, b, i + 1, aw + w[i], av + v[i])
-	let no = process(w, v, b, i + 1, aw, av)
-	return Math.max(yes, no)
+const first = (arr, l, r) => {
+	if (l == r) return arr[l]
+	return Math.max(arr[l] + second(arr, l + 1, r), arr[r] + second(arr, l, r - 1))	
 }
 
-const weights1 = [3, 2, 4, 7];
-const values1 = [5, 6, 3, 19];
-let bag1 = 11;
-
-console.log(knapsack(weights1, values1, bag1))
-
-const knapsackClassic = (weights, values, bag) => {
-	if (!weights || !values || bag <= 0) return 0
-	return processClassic(weights, values, bag, 0)
+const second = (arr, l, r) => {
+	if (l == r) return 0
+	return Math.min(first(arr, l + 1, r), first(arr, l, r - 1))
 }
 
-// r: rest 剩下能装多重
-const processClassic = (w, v, r, i) => {
-	if (w[i] > r) return -1 // 此方案行不通，-1 为状态
-	if (i == w.length) return 0
-
-	let yes = -1 // 先假设当前方案行不通
-	let yesNext = processClassic(w, v, r - w[i], i + 1)
-	if (yesNext != -1) yes = v[i] + yesNext // 若装当前货物，后面的决策都不是 -1，说明行得通
-	
-	let no = processClassic(w, v, r, i + 1)
-
-	return Math.max(yes, no)
-}
-
-console.log(knapsackClassic(weights1, values1, bag1))
+const arr1 = [4, 7, 9, 5]
+console.log(cardsGame(arr1))
