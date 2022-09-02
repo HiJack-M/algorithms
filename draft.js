69b83587acdd44dffaa498d0fc53272e18683725
HiJack <hijack.sherlock@live.com>
2022-09-02 21:10:29 +0800

    refactor(dynamic programming): robotWalk

--- draft.js
+++ draft.js
@@ -1,64 +1,27 @@
-// N皇后问题是指在 N*N 的棋盘上要摆 N 个皇后，要求任何两个皇后不同行、不同列， 也不在同一条斜线上。给定一个整数 n，返回 n 皇后的摆法有多少种。
+// 假设有排成一行的 N 个位置，记为 1~N ，N 一定大于或等于 2
+// 开始时机器人在其中的 M 位置上(M 一定是 1~N 中的一个)
+// 如果机器人来到 1 位置，那么下一步只能往右来到 2 位置；
+// 如果机器人来到 N 位置，那么下一步只能往左来到 N-1 位置；
+// 如果机器人来到中间位置，那么下一步可以往左走或者往右走；
+// 规定机器人必须走 K 步，最终能来到 P 位置(P 也是 1~N 中的一个)的方法有多少种
+// 给定四个参数 N、M、K、P，返回方法数。
 
-const NQueens = (N) =>{
-	if (!N || N <= 0) return 0
-	const arrPut = [] // 缓存已经放好的点
-	return process(N, 0, arrPut)
+const robotWalkRecursion = (N, M, K, P) => {
+	if (N < 2 || M < 1 || M > N || K < 0 || P < 1 || P > N) return 0
+	return processRecursion(N, M, K, P)
 }
 
-// 在第 i 行做决定
-const process = (n, i, arrPut) => {
-	// base case
-	if (i == n) return 1
-
-	let result = 0
-	for (let j = 0; j < n; j++) {
-		if (isOkToPutHere(i, j, arrPut)) {
-			arrPut.push(j)
-			result += process(n, i + 1, arrPut)
-			arrPut.pop()
-		}
+const processRecursion = (N, cur, rest, P) => {
+	if (rest == 0) {
+		return cur == P ? 1 : 0
 	}
-	return result
-}
-
-// 第 i 行摆在第 j 位置行不行
-const isOkToPutHere = (i, j, arrPut) => {
-	// arrPut.length 已经摆了多少行
-	// arrPut[i] -> j: 第 i 行摆在第 j 个位置
-	for (let k = 0; k < arrPut.length; k++) {
-		if (arrPut[k] == j) return false // 同列
-		if (Math.abs(k - i) == Math.abs(arrPut[k] - j)) return false
+	if (cur == 1) {
+		return processRecursion(N, cur + 1, rest - 1, P)
 	}
-	return true
-}
-
-console.log('normal: ', NQueens(5))
-
-// test git reset
-
-// 优化常数项
-const NQueensBit = (N) => {
-	if (!N || N < 1) return 0
-	// limit 不变，最终摆满皇后，N 个 1（玩二进制数）
-	let limit = N > 32 ? -1 : (1 << N) - 1
-	return processBit(limit, 0, 0, 0)
-}
-
-const processBit = (limit, colLim,leftLim, rightLim) => {
-	// base case
-	if (colLim == limit) return 1
-
-	let res = 0
-	// 所有位置中，还不是 1 的位置
-	let pos = limit & ~(colLim | leftLim | rightLim)
-	// 每次拿最右侧的 1 位置尝试，尝试完去掉，最终尝试完所有的 1，pos 为 0
-	while (pos != 0) {
-		let mostRightOne = pos & (~pos + 1)
-		pos = pos - mostRightOne
-		res += processBit(limit, colLim + mostRightOne, (leftLim + mostRightOne) << 1, (rightLim + mostRightOne) >> 1)
+	if (cur == N) {
+		return processRecursion(N, cur - 1, rest - 1, P)
 	}
-	return res
+	return processRecursion(N, cur - 1, rest - 1, P) + processRecursion(N, cur + 1, rest - 1, P)
 }
 
-console.log('bit: ', NQueensBit(5))
+console.log('recursion: ', robotWalkRecursion(7, 3, 3, 2))
