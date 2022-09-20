16e6bad41e080d6f1834c3b6c3d2f8cdfa995140
HiJack <hijack.sherlock@live.com>
2022-09-20 12:49:40 +0800

    refactor(leetcode hot 100): no.2

--- draft.js
+++ draft.js
@@ -1,17 +1,38 @@
-// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target  的那 两个 整数，并返回它们的【数组下标】。
-// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
-// 你可以按任意顺序返回答案。
+// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 
-var twoSum = function(nums, target) {
-	if (!nums || !target) return null
-	let map = {}
-	for (let i = 0; i < nums.length; i++) {
-		let matchNum = target - nums[i]
-		if (map[matchNum] != undefined) {
-			return [i, map[matchNum]]
-		} else {
-			map[nums[i]] = i
-		}
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+	if (!l1 || !l2) return null
+	let carryBit = 0
+	let p1 = l1
+	let p2 = l2
+	let pRes = new ListNode(0)
+	const dummyHead = pRes
+	while (p1 || p2) {
+		let v1 = p1 ? p1.val : 0
+		let v2 = p2 ? p2.val : 0
+		let item = v1 + v2 + carryBit
+		pRes.next = new ListNode(item % 10)
+		pRes = pRes.next
+		carryBit = Math.floor(item / 10)
+		p1 = p1 ? p1.next : null
+		p2 = p2 ? p2.next : null
 	}
-	return null
-}
+	if (carryBit != 0) {
+		pRes.next = new ListNode(carryBit)
+	}
+	return dummyHead.next
+};
