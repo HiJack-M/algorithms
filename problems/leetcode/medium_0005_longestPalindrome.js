18e361c75a2ae04765599791a9c658a01416c2f3
HiJack <hijack.sherlock@live.com>
2022-09-22 15:28:36 +0800

    feat(leetcode hot 100): No.5 Dynamic Programming way

--- problems/leetcode/medium_0005_longestPalindrome.js
+++ problems/leetcode/medium_0005_longestPalindrome.js
@@ -40,3 +40,41 @@ const verifyTwoSides = (s, i, ri) => {
 }
 
 console.log(longestPalindrome('aacabdkacaa'))
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindromeDp = function(s) {
+	let ans = ''
+    if (!s) return ans
+
+	ans = s[0]
+	let N = s.length
+	let Dp = new Array(N)
+	for (let i = 0; i < N; i++) {
+		Dp[i] = new Array(N)
+		Dp[i].fill(false)
+		Dp[i][i] = true // 单个字符为回文
+	}
+
+	// Dp 表的左下半部分没用
+	for (let i = 1; i < N; i++) {
+		let a = 0
+		let b = i
+		while (a < N && b < N) {
+			if (s[a] == s[b]) {
+				Dp[a][b] = Dp[Math.min(a + 1, b - 1)][b - 1] ? true : false
+				if (Dp[a][b] && b - a + 1 > ans.length) {
+					ans = s.substring(a, b + 1)
+				}
+			} else {
+				Dp[a][b] = false
+			}
+			a++
+			b++
+		}
+	}
+	return ans
+};
