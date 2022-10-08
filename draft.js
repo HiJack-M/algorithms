a7f09d2da8d6f03d3eb6bce7686fb0153e1f6ebb
HiJack <hijack.sherlock@live.com>
2022-10-08 17:10:27 +0800

    feat(leetcode hot 100): 31 next permutation

--- draft.js
+++ draft.js
@@ -1,35 +1,55 @@
-// 22. Generate Parentheses
+// 31. Next Permutation
 
-// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+const swap = (arr, i, j) => {
+  let temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+
+const reverse = (arr, start, end) => {
+  while (start < end) {
+    swap(arr, start++, end--)
+  }
+}
 
 /**
- * @param {number} n
- * @return {string[]}
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-var generateParenthesis = function (n) {
-  if (n == 0) return []
-  let ans = []
-  process(n, n, '', ans)
-  return ans
+var nextPermutation = function (nums) {
+  if (!nums || nums.length == 0) return nums
+
+  // 找到第一组顺序对，取到小值 (较小值尽量靠右)
+  let small = -1
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      small = i
+      break
+    }
+  }
+  // 找到逆序中的刚好比较小值大的最小的那个值
+  let big = -1
+  for (let i = nums.length - 1; i > small; i--) {
+    if (nums[i] > nums[small]) {
+      big = i
+      break
+    }
+  }
+  if (small != -1 && big != -1) {
+    swap(nums, small, big)
+  }
+  // 从下标为 small + 1 出开始顺序排序
+  reverse(nums, small + 1, nums.length - 1)
 }
-
-// 用剩余量来控制递归
-const process = (restLeft, restRight, path, ans) => {
-  if (restLeft == 0 && restRight == 0) {
-    ans.push(path)
-    return
-  }
-
-  if (restLeft > 0) {
-    let curPath = path + '('
-    process(restLeft - 1, restRight, curPath, ans)
-  }
-
-  // 以确保括号是有效的
-  if (restRight > restLeft) {
-    let curPath = path + ')'
-    process(restLeft, restRight - 1, curPath, ans)
-  }
-}
-
-console.log(generateParenthesis(3))
