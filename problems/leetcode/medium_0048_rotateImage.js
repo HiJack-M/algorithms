2ca8625765fa885a146f123ab3c53614b639360d
HiJack <hijack.sherlock@live.com>
2022-10-14 12:26:11 +0800

    feat(leetcode hot 100): 48 rotate image

--- problems/leetcode/medium_0048_rotateImage.js
+++ problems/leetcode/medium_0048_rotateImage.js
@@ -27,20 +27,3 @@ const circleSwap = (matrix, i, j) => {
   matrix[j][n - i] = matrix[i][j]
   matrix[i][j] = temp
 }
-
-const matrix1 = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]
-
-rotate(matrix1)
-
-const matrix2 = [
-  [5, 1, 9, 11],
-  [2, 4, 8, 10],
-  [13, 3, 6, 7],
-  [15, 14, 12, 16],
-]
-
-rotate(matrix2)
