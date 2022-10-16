cfd39f7ac45ee69319dd017bc5af13943ae1a004
HiJack <hijack.sherlock@live.com>
2022-10-16 19:10:03 +0800

    feat(leetcode hot 100): 96 unique BSTs

--- draft.js
+++ draft.js
@@ -1,90 +1,24 @@
-// 79. Word Search
+// 96. Unique Binary Search Trees
 
-// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+// Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
 
 /**
- * @param {character[][]} board
- * @param {string} word
- * @return {boolean}
+ * @param {number} n
+ * @return {number}
  */
-var exist = function (board, word) {
-  if (!board || board.length == 0 || !word) return false
+var numTrees = function (n) {
+  const G = new Array(n + 1).fill(0)
 
-  let m = board.length
-  let n = board[0].length
+  G[0] = 1
+  G[1] = 1
 
-  let visitedGrid = []
-  for (let i = 0; i < m; i++) {
-    visitedGrid[i] = new Array(n)
-    visitedGrid[i].fill(false)
-  }
-
-  let exist = false
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      let curExist = check(board, word, 0, i, j, visitedGrid)
-      if (curExist) {
-        exist = curExist
-        break
-      }
-    }
-  }
-  return exist
-}
-
-const check = (board, word, index, x, y, visitedGrid) => {
-  if (word[index] != board[x][y]) {
-    return false
-  } else if (index == word.length - 1) {
-    return true
-  }
-
-  visitedGrid[x][y] = true
-
-  let exist = false
-  let direction = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ]
-  for (const [dx, dy] of direction) {
-    let newX = x + dx
-    let newY = y + dy
-    if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length) {
-      if (!visitedGrid[newX][newY]) {
-        let curExist = check(board, word, index + 1, newX, newY, visitedGrid)
-        if (curExist) {
-          exist = curExist
-          break
-        }
-      }
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      G[i] += G[j - 1] * G[i - j]
     }
   }
 
-  visitedGrid[x][y] = false
-  return exist
+  return G[n]
 }
 
-const board1 = [
-  ['A', 'B', 'C', 'E'],
-  ['S', 'F', 'C', 'S'],
-  ['A', 'D', 'E', 'E'],
-]
-const word1 = 'ABCB' // false
-const word11 = 'ABCCED' // true
-
-const board2 = [
-  ['A', 'B', 'C', 'E'],
-  ['S', 'F', 'C', 'S'],
-  ['A', 'D', 'E', 'E'],
-]
-const word2 = 'SEE' // true
-
-const board3 = [['a', 'a']]
-const word3 = 'aa' // true
-
-console.log(exist(board1, word1))
-console.log(exist(board1, word11))
-console.log(exist(board2, word2))
-console.log(exist(board3, word3))
+console.log(numTrees(3))
