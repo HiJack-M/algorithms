222c785381801040eb7e33f1209a4f56bc157e91
HiJack <hijack.sherlock@live.com>
2022-10-17 15:46:14 +0800

    feat(leetcode hot 100): 0096 unique BSTs

---
+++ problems/leetcode/medium_0096_numBSTs.js
@@ -0,0 +1,22 @@
+// 96. Unique Binary Search Trees
+
+// Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+  const G = new Array(n + 1).fill(0)
+
+  G[0] = 1
+  G[1] = 1
+
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      G[i] += G[j - 1] * G[i - j]
+    }
+  }
+
+  return G[n]
+}
