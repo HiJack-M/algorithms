90d2aa022e0fb3b71a375e8b72937cf6a17cee90
HiJack <hijack.sherlock@live.com>
2022-10-22 23:21:46 +0800

    refactor(leetcode hot 100): 169 process2

--- draft.js
+++ draft.js
@@ -1,1 +1,27 @@
-// did nothing today...
+// 169. Majority Element
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  if (!nums || nums.length == 0) return null
+
+  let winner = nums[0]
+  let count = 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] == winner) {
+      count++
+    } else if (count == 0) {
+      winner = nums[i]
+      count++
+    } else {
+      count--
+    }
+  }
+  return winner
+}
