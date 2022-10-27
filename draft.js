9c1d2791eadf6ac2196db5ba3d001478b3c998d9
HiJack <hijack.sherlock@live.com>
2022-10-27 15:57:59 +0800

    refactor(leetcode hot 100): 160 review way1

--- draft.js
+++ draft.js
@@ -1,26 +1,53 @@
-// 152. Maximum Product Subarray
+// 160. Intersection of Two Linked Lists
+
+// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
 /**
- * @param {number[]} nums
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var maxProduct = function (nums) {
-  if (!nums || nums.length == 0) return 0
 
-  let ans = nums[0]
-  let min = nums[0]
-  let max = nums[0]
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null
 
-  // 以 i 结尾的最小积、最大积
-  for (let i = 1; i < nums.length; i++) {
-    let curMin = Math.min(nums[i], Math.min(nums[i] * min, nums[i] * max))
-    let curMax = Math.max(nums[i], Math.max(nums[i] * min, nums[i] * max))
-    min = curMin // 这里不比较，因为一定要以 i 结尾，不断，所以每个都只能取当下
-    max = curMax
-    ans = Math.max(max, ans)
+  let p1 = headA
+  let p2 = headB
+  let count = 0
+  while (p1) {
+    p1 = p1.next
+    count++
+  }
+  while (p2) {
+    p2 = p2.next
+    count--
   }
 
-  return ans
-}
+  p1 = headA
+  p2 = headB
+  if (count > 0) {
+    while (count > 0) {
+      p1 = p1.next
+      count--
+    }
+  } else if (count < 0) {
+    while (count < 0) {
+      p2 = p2.next
+      count++
+    }
+  }
 
-console.log(maxProduct([0, 2]))
+  while (p1 != p2) {
+    p1 = p1.next
+    p2 = p2.next
+  }
+
+  return p1
+}
