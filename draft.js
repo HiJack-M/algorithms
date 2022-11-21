b04512bafc28e64c52ccbd0085ab38196020ae91
HiJack <hijack.sherlock@live.com>
2022-11-21 11:28:29 +0800

    refactor(300 LIS process1.1435962):

--- draft.js
+++ draft.js
@@ -1,84 +1,37 @@
-// 322. Coin Change
+// 300. Longest Increasing Subsequence
+
+// Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
 /**
- * @param {number[]} coins
- * @param {number} amount
+ * @param {number[]} nums
  * @return {number}
  */
-var coinChange = function (coins, amount) {
-  if (!coins || coins.length == 0 || amount < 0) return -1
-  if (amount == 0) return 0
+var lengthOfLIS = function (nums) {
+  if (!nums || nums.length == 0) return 0
 
-  let Dp = new Array(coins.length + 1)
-  for (let i = 0; i < Dp.length; i++) {
-    Dp[i] = new Array(amount + 1)
-  }
+  let ends = [] // ends[i]: i + 1 长度的 LIS 的最小结尾值
+  ends[0] = nums[0]
+  let right = 0 // LIS 的有效长度，ends 的有效区
+  let longest = 1
 
-  return process(coins, 0, amount, Dp)
-}
-
-// 返回使用的张数
-const process = (coins, index, rest, Dp) => {
-  if (Dp[index][rest] != undefined) {
-    return Dp[index][rest]
-  }
-
-  // base case
-  if (index == coins.length) {
-    if (rest == 0) {
-      Dp[index][0] = 0
-      return 0
-    } else {
-      return -1
-    }
-  }
-
-  let fewestNeeds = Infinity
-
-  for (let i = 0; coins[index] * i <= rest; i++) {
-    let nextNeeds = process(coins, index + 1, rest - coins[index] * i, Dp)
-
-    if (nextNeeds != -1) {
-      fewestNeeds = Math.min(fewestNeeds, nextNeeds + i)
-    }
-  }
-
-  Dp[index][rest] = fewestNeeds == Infinity ? -1 : fewestNeeds
-  return Dp[index][rest]
-}
-
-const coins1 = [1, 2, 5]
-console.log(coinChange(coins1, 11))
-
-var coinChangeDp = (coins, amount) => {
-  if (!coins || coins.length == 0 || amount < 0) return -1
-  if (amount == 0) return 0
-
-  let N = coins.length
-  let Dp = new Array(N + 1)
-  for (let i = 0; i < Dp.length; i++) {
-    Dp[i] = new Array(amount + 1)
-  }
-  Dp[N].fill(-1)
-  Dp[N][0] = 0
-
-  for (let index = N - 1; index >= 0; index--) {
-    for (let rest = 0; rest <= amount; rest++) {
-      let fewestNeeds = Infinity
-
-      for (let i = 0; coins[index] * i <= rest; i++) {
-        let nextNeeds = Dp[index + 1][rest - coins[index] * i]
-
-        if (nextNeeds != -1) {
-          fewestNeeds = Math.min(fewestNeeds, nextNeeds + i)
-        }
+  let l = 0
+  let r = 0
+  let mid = 0
+  for (let i = 1; i < nums.length; i++) {
+    l = 0
+    r = right
+    while (l <= r) {
+      m = l + ((r - l) >> 1)
+      if (nums[i] < ends[m]) {
+        r = m - 1
+      } else {
+        l = m + 1
       }
-
-      Dp[index][rest] = fewestNeeds == Infinity ? -1 : fewestNeeds
     }
+    right = Math.max(right, l)
+    ends[l] = nums[i]
+    longest = Math.max(longest, l + 1)
   }
 
-  return Dp[0][amount]
+  return longest
 }
-
-console.log(coinChangeDp(coins1, 11))
