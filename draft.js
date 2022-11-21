c2f6732b9cffcd048be76ad25d22f26b7849d3a6
HiJack <hijack.sherlock@live.com>
2022-11-21 18:22:22 +0800

    perf(leetcode hot 100): 448 findDisappearedNumbers 注意取模的时机!!!

--- draft.js
+++ draft.js
@@ -27,3 +27,28 @@ console.log(findDisappearedNumbers(nums1))
 
 const nums2 = [1, 1]
 console.log(findDisappearedNumbers(nums2))
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbersSaveSpace = function (nums) {
+  let ans = []
+  if (!nums || nums.length == 0) return ans
+
+  let N = nums.length
+  for (let num of nums) {
+    let x = (num - 1) % N // 先减一，再取模，可找到最后一个下标，若先取模，n % n 为 0，而后再减 1，就定位到 nums[-1] 去了
+    nums[x] += N
+  }
+  for (let [i, num] of nums.entries()) {
+    if (num <= N) {
+      ans.push(i + 1)
+    }
+  }
+
+  return ans
+}
+
+console.log(findDisappearedNumbersSaveSpace(nums1))
+console.log(findDisappearedNumbersSaveSpace(nums2))
