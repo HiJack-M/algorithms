c2d798ddd934bc6d585c15f4a0ac224bfe598d1b
HiJack <hijack.sherlock@live.com>
2022-11-25 13:24:40 +0800

    refactor(leetcode top interview): 13 [process 2]

--- draft.js
+++ draft.js
@@ -1,53 +1,29 @@
-// 394. Decode String
-
-// Given an encoded string, return its decoded string.
-
-// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
-
-// You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
-
-// The test cases are generated so that the length of the output will never exceed 105.
-
 /**
  * @param {string} s
- * @return {string}
+ * @return {number}
  */
-var decodeString = function (s) {
-  let path = ''
-  if (!s) return path
+var romanToInt = function (s) {
+  let romanObj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
 
-  return process(s, 0).str
-}
-
-// 返回值 { str, end }
-const process = (s, index) => {
-  let str = ''
-  let times = 0
-  while (index < s.length && s[index] !== ']') {
-    if (/[a-zA-Z]/.test(s[index])) {
-      str += s[index++]
-    } else if (/[0-9]/.test(s[index])) {
-      times = times * 10 + +s[index++]
+  let point = 0
+  let sum = 0
+  while (point < s.length) {
+    if (point < s.length - 1 && romanObj[s[point]] < romanObj[s[point + 1]]) {
+      sum -= romanObj[s[point++]]
     } else {
-      // str[index] = '['
-      let next = process(s, index + 1)
-      str += timesStr(times, next.str)
-      index = next.end + 1
-      times = 0
+      sum += romanObj[s[point++]]
     }
   }
 
-  return { str, end: index }
+  return sum
 }
 
-const timesStr = (times, str) => {
-  let ans = ''
-  while (times > 0) {
-    ans += str
-    times--
-  }
-  return ans
-}
-
-const testStr1 = '3[a]2[bc]'
-console.log(decodeString(testStr1))
+console.log(romanToInt('MCMXCIV'))
