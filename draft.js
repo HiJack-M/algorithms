f69b7c01b4bbf0bbebfb13d60aa34117f20b6a79
HiJack <hijack.sherlock@live.com>
2022-11-25 13:50:46 +0800

    refactor(leetcode top interview): 14 [process 2]

--- draft.js
+++ draft.js
@@ -1,29 +1,36 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function (s) {
-  let romanObj = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  }
+// 14. Longest Common Prefix
 
-  let point = 0
-  let sum = 0
-  while (point < s.length) {
-    if (point < s.length - 1 && romanObj[s[point]] < romanObj[s[point + 1]]) {
-      sum -= romanObj[s[point++]]
-    } else {
-      sum += romanObj[s[point++]]
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs || strs.length == 0) return ''
+  if (strs?.length == 1) return strs[0]
+
+  let common = strs[0] // 先将公共前缀设置为第一个串
+  for (let i = 1; i < strs.length; i++) {
+    // 从第二个串开始比较，如果比较的串为空，那公共前缀就肯定是空
+    if (strs[i] === '') return ''
+
+    // j 为比较指针
+    for (let j = 0; j < strs[i].length; j++) {
+      if (common[j] != strs[i][j]) {
+        common = strs[i].substring(0, j) // 字母不同，两串的公共前缀为相同部分，替换 common 变量
+        break
+      }
+      if (j == strs[i].length - 1 && common[j] == strs[i][j]) {
+        common = strs[i]
+      }
     }
   }
 
-  return sum
+  return common
 }
 
-console.log(romanToInt('MCMXCIV'))
+const strs1 = ['flower', 'flow', 'flight']
+console.log(longestCommonPrefix(strs1))
--- problems/leetcode/easy_0014_longestCommonPrefix.js
+++ problems/leetcode/easy_0014_longestCommonPrefix.js
@@ -6,23 +6,30 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-if (!strs || strs.length == 0) return ''
+var longestCommonPrefix = function (strs) {
+  if (!strs || strs.length == 0) return ''
   if (strs?.length == 1) return strs[0]
-  let common = strs[0]
+
+  let common = strs[0] // 先将公共前缀设置为第一个串
+  // 从第二个串开始比较，如果比较的串为空，那公共前缀就肯定是空
   for (let i = 1; i < strs.length; i++) {
     // 若某个字符串为空
     if (strs[i] == '') return ''
+
+    // j 为比较指针
     for (let j = 0; j < strs[i].length; j++) {
       if (common[j] != strs[i][j]) {
-        common = strs[i].substring(0, j)
+        common = strs[i].substring(0, j) // 字母不同，两串的公共前缀为相同部分，替换 common 变量
         break
       }
       // 若直到某个字符串的最后一个字符都一致
-      if ((j == strs[i].length - 1) && common[j] == strs[i][j]) {
+      if (j == strs[i].length - 1 && common[j] == strs[i][j]) {
         common = strs[i]
       }
     }
   }
   return common
 }
+
+const strs1 = ['flower', 'flow', 'flight']
+console.log(longestCommonPrefix(strs1))
