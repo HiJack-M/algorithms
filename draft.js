5a249d20c11373699fb8b18237388dc57bf4590e
HiJack <hijack.sherlock@live.com>
2022-12-06 15:00:45 +0800

    refactor(Huffman Coding): review

--- draft.js
+++ draft.js
@@ -1,61 +1,77 @@
-// 38. Count and Say
+/* 一块金条切成两半，是需要花费和长度数值一样的铜板的。
+ * 比如长度为20的金条，不管怎么切，都要花费20个铜板。
+ * 一群人想整分整块金条，怎么分最省铜板?
+ *
+ * 例如,给定数组{10,20,30}，代表一共三个人，整块金条长度为60，金条要分成10，20，30三个部分。
+ * 如果先把长度60的金条分成10和50，花费60; 再把长度50的金条分成20和30，花费50;一共花费110铜板。
+ * 但如果先把长度60的金条分成30和30，花费60;再把长度30金条分成10和20， 花费30;一共花费90铜板。
+ *
+ * 输入一个数组，返回分割的最小代价。 */
 
-// The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
-
-// countAndSay(1) = "1"
-// countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
-
-// To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
-
-// Given a positive integer n, return the nth term of the count-and-say sequence.
-
-// Constraints: 1 <= n <= 30
-
-/**
- * @param {number} n
- * @return {string}
- */
-var countAndSay = function (n) {
-  if (n == 0) return null
-
-  let Dp = new Array(n + 1)
-  Dp[0] = 'dummy'
-  Dp[1] = '1'
-
-  if (n == 1) return Dp[1]
-
-  let pre
-  let k
-  let curLetter
-  let curCount
-  let curAns
-  for (let i = 2; i <= n; i++) {
-    // say
-    pre = Dp[i - 1]
-    k = 0
-    curAns = ''
-    curLetter = null
-    curCount = 0
-    while (k < pre.length) {
-      if (curLetter == null) {
-        curLetter = pre[k]
-        curCount = 1
-      } else if (pre[k] == curLetter) {
-        curCount++
-      } else {
-        curAns = curAns + curCount + curLetter
-        curCount = 1
-        curLetter = pre[k]
+class MinHeap {
+  constructor(arr) {
+    this.heap = []
+    if (arr && arr.length > 0) {
+      for (let item of arr) {
+        this.heapInsert(item, this.heap.length)
       }
-      if (k == pre.length - 1) {
-        curAns = curAns + curCount + curLetter
-      }
-      k++
     }
-    Dp[i] = curAns
   }
 
-  return Dp[n]
+  swap(i, j) {
+    let temp = this.heap[i]
+    this.heap[i] = this.heap[j]
+    this.heap[j] = temp
+  }
+
+  heapInsert(val, index) {
+    this.heap.push(val)
+    while (this.heap[index] < this.heap[Math.floor((index - 1) / 2)]) {
+      this.swap(index, Math.floor((index - 1) / 2))
+      index = Math.floor((index - 1) / 2)
+    }
+  }
+
+  heapify(index, end) {
+    let left = index * 2 + 1
+    while (left <= end) {
+      let smallestI = this.heap[index] < this.heap[left] ? index : left
+      if (left + 1 <= end) {
+        smallestI = this.heap[smallestI] < this.heap[left + 1] ? smallestI : left + 1
+      }
+      if (smallestI == index) break
+
+      this.swap(smallestI, index)
+      index = smallestI
+      left = index * 2 + 1
+    }
+  }
+
+  poll() {
+    this.swap(0, this.heap.length - 1)
+    let res = this.heap.pop()
+    this.heapify(0, this.heap.length - 1)
+    return res
+  }
 }
 
-console.log(countAndSay(4))
+const lessMoney = (arr) => {
+  if (arr == null || arr.length < 2) {
+    return 0
+  }
+
+  let coinHeap = new MinHeap(arr)
+  let ans = 0
+  while (coinHeap.heap.length > 1) {
+    let a = coinHeap.poll()
+    let b = coinHeap.poll()
+    let newVal = a + b
+    ans += newVal
+    coinHeap.heapInsert(newVal)
+  }
+
+  return ans
+}
+
+const arr1 = [26, 40, 33, 34, 45, 45, 41, 29, 40]
+console.log(lessMoney(arr1))
--- lecture-code/class09_lessMoneySplitGold.js
+++ lecture-code/class09_lessMoneySplitGold.js
@@ -9,37 +9,37 @@
  * 输入一个数组，返回分割的最小代价。 */
 
 const lessMoney1 = (arr) => {
-    if (arr == null || arr.length == 0) {
-        return 0;
-    }
-    return process(arr, 0);
-} 
+  if (arr == null || arr.length == 0) {
+    return 0
+  }
+  return process(arr, 0)
+}
 
 // arr 还要分成多少长度的数组
 // 之前已经花费的铜板
-const process = (arr, pre)  => {
-    if (arr.length == 1) {
-        return pre;
+const process = (arr, pre) => {
+  if (arr.length == 1) {
+    return pre
+  }
+  let cost = Infinity
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      cost = Math.min(cost, process(copyAndMerge(arr, i, j), pre + arr[i] + arr[j]))
     }
-    let cost = Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            cost = Math.min(cost, process(copyAndMerge(arr, i, j), pre + arr[i] + arr[j]));
-        }
-    }
-    return cost;
+  }
+  return cost
 }
 
 // 除下标 i 和 j 之外的数，全部直接拷入新数组，i + j 合并再放入新数组
 const copyAndMerge = (arr, i, j) => {
-    let ans = []
-    for (let a = 0; a < arr.length; a++) {
-        if (a != i && a != j) {
-        ans.push(arr[a])
-        }
+  let ans = []
+  for (let a = 0; a < arr.length; a++) {
+    if (a != i && a != j) {
+      ans.push(arr[a])
     }
-    ans.push(arr[i] + arr[j])
-    return ans
+  }
+  ans.push(arr[i] + arr[j])
+  return ans
 }
 
 /* const arr1 = [ 26, 40, 33, 34, 45, 45, 41, 29, 40 ];
@@ -47,99 +47,99 @@ const copyAndMerge = (arr, i, j) => {
 
 // 建立一个小顶堆
 class PriorityQueue {
-    constructor() {
-        this.queue = [];
-    }
+  constructor() {
+    this.queue = []
+  }
 
-    add(value) {
-        this.queue.unshift(value); // 如果只用 heapify 操作，那加入的时候，就一定要从下标 0 开始！！！
-        this.heapify();
-    }
+  add(value) {
+    this.queue.unshift(value) // 如果只用 heapify 操作，那加入的时候，就一定要从下标 0 开始！！！
+    this.heapify()
+  }
 
-    swap(i, j) {
-        let temp = this.queue[i];
-        this.queue[i] = this.queue[j];
-        this.queue[j] = temp;
-    }
+  swap(i, j) {
+    let temp = this.queue[i]
+    this.queue[i] = this.queue[j]
+    this.queue[j] = temp
+  }
 
-    heapify() {
-        if (this.queue.length <= 1) {
-            return;
-        }
-        let index = 0;
-        let left = index * 2 + 1;
-        while (left < this.queue.length) {
-            let smallest = left + 1 < this.queue.length && this.queue[left + 1] < this.queue[left] ? left + 1 : left;
-            smallest = this.queue[smallest] < this.queue[index] ? smallest : index;
-            if (smallest == index) {
-                break;
-            }
-            this.swap(index, smallest);
-            index = smallest;
-            left = index * 2 + 1;
-        }
+  heapify() {
+    if (this.queue.length <= 1) {
+      return
     }
+    let index = 0
+    let left = index * 2 + 1
+    while (left < this.queue.length) {
+      let smallest =
+        left + 1 < this.queue.length && this.queue[left + 1] < this.queue[left] ? left + 1 : left
+      smallest = this.queue[smallest] < this.queue[index] ? smallest : index
+      if (smallest == index) {
+        break
+      }
+      this.swap(index, smallest)
+      index = smallest
+      left = index * 2 + 1
+    }
+  }
 
-    poll() {
-        if (this.queue.length == 0) {
-            throw Error('queue is empty!');
-        }
-        let value = this.queue.shift();
-        if (this.queue.length > 1) {
-            this.heapify();
-        }
-        return value; 
+  poll() {
+    if (this.queue.length == 0) {
+      throw Error('queue is empty!')
     }
+    let value = this.queue.shift()
+    if (this.queue.length > 1) {
+      this.heapify()
+    }
+    return value
+  }
 }
 
 // 贪心算法解
 const lessMoney2 = (arr) => {
-    if (arr == null || arr.length == 0) {
-        return 0;
-    }
-    // 哈夫曼树：
-    // 1. 建出小根堆
-    // 2. 弹出两个，相加，相加值 val 累加到 sum (每个非叶子结点相加)
-    // 3. val 再入堆，再循环2
-    // 直到堆中只剩一个数时，返回 sum
-    let queue = new PriorityQueue();
-    for (let i = 0; i < arr.length; i++) {
-        queue.add(arr[i]);
-    }
-    let sum = 0;
-    while (queue.queue.length > 1) {
-        let val = queue.poll() + queue.poll();
-        sum += val;
-        queue.add(val);
-    }
-    return sum;
+  if (arr == null || arr.length == 0) {
+    return 0
+  }
+  // 哈夫曼树：
+  // 1. 建出小根堆
+  // 2. 弹出两个，相加，相加值 val 累加到 sum (每个非叶子结点相加)
+  // 3. val 再入堆，再循环2
+  // 直到堆中只剩一个数时，返回 sum
+  let queue = new PriorityQueue()
+  for (let i = 0; i < arr.length; i++) {
+    queue.add(arr[i])
+  }
+  let sum = 0
+  while (queue.queue.length > 1) {
+    let val = queue.poll() + queue.poll()
+    sum += val
+    queue.add(val)
+  }
+  return sum
 }
-
 
 /* const arr2 = [ 26, 40, 33, 34, 45, 45, 41, 29, 40 ];
  * console.log(lessMoney2(arr2)); */
 
 const generateRandomArr = (maxLen, maxValue) => {
-    let arr = new Array(parseInt((maxLen + 1) * Math.random()));
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = parseInt((maxValue * Math.random() + 1));
-    }
-    return arr;
+  let arr = new Array(parseInt((maxLen + 1) * Math.random()))
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(maxValue * Math.random() + 1)
+  }
+  return arr
 }
 
 const testMachine = (maxLen, maxValue, maxTime) => {
-    for (let i = 1; i < maxTime; i++) {
-        let arr = generateRandomArr(maxLen, maxValue);
-        let arrCopy = [...arr];
-        if (lessMoney1(arr) != lessMoney2(arrCopy)) {
-            console.log('arr error: ', arr)
-            console.log(i);
-            console.log('Oops!');
-            return;
-        }
+  for (let i = 1; i < maxTime; i++) {
+    let arr = generateRandomArr(maxLen, maxValue)
+    let arrCopy = [...arr]
+    if (lessMoney1(arr) != lessMoney2(arrCopy)) {
+      console.log('arr error: ', arr)
+      console.log(i)
+      console.log('Oops!')
+      return
     }
-    console.log('Finished!');
+  }
+  console.log('Finished!')
 }
 
-testMachine(5, 50, 100);
+testMachine(5, 50, 100)
 // 数组长度超过5，lessMoney1 就能明显看到延迟
