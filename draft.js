28e9ecc2a0f0ff9c2e321935b778682a88b91030
HiJack <hijack.sherlock@live.com>
2023-03-12 15:00:48 +0800

    refactor(leetcode review):
    114. Flatten Binary Tree to Linked List

--- draft.js
+++ draft.js
@@ -1,73 +1,64 @@
-// 105. Construct Binary Tree from Preorder and Inorder Traversal
+// 114. Flatten Binary Tree to Linked List
 
-// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+// Given the root of a binary tree, flatten the tree into a "linked list":
 
-// Definition for a binary tree node.
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val
-  this.left = left === undefined ? null : left
-  this.right = right === undefined ? null : right
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+  if (!root || (!root.left && !root.right)) return
+
+  let dummyHead = new TreeNode(0)
+  let p = dummyHead
+
+  let helpQueue = []
+  helpQueue.push(root)
+
+  while (helpQueue.length > 0) {
+    let node = helpQueue.pop()
+
+    if (node.right) helpQueue.push(node.right)
+    if (node.left) helpQueue.push(node.left)
+
+    p.right = node
+    node.left = null
+    p = node
+  }
+
+  dummyHead = dummyHead.right
 }
 
 /**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
  */
-var buildTree = function (preorder, inorder) {
-  if (
-    !preorder ||
-    !inorder ||
-    preorder.length === 0 ||
-    inorder.length === 0 ||
-    preorder.length !== inorder.length
-  )
-    return null
+var flatten_O1 = function (root) {
+  if (!root) return
 
-  let rootVal = preorder.shift()
-  let root = new TreeNode(rootVal)
-  let inPivot = inorder.indexOf(rootVal)
-
-  let leftPreorder = preorder.splice(0, inPivot) // 取出 preorder 的左部分
-  // 右半部分就是原数组
-
-  let leftInorder = inorder.splice(0, inPivot) // 取出 inorder 的左部分
-  inorder.shift() // 把已经建立 root 的 pivot 去掉
-  // 右半部分就是原数组
-
-  root.left = buildTree(leftPreorder, leftInorder)
-  root.right = buildTree(preorder, inorder)
-
-  return root
-}
-
-const preorder1 = [3, 9, 20, 15, 7]
-const inorder1 = [9, 3, 15, 20, 7]
-console.log(buildTree(preorder1, inorder1))
-
-/**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTreeWasteArr = function (preorder, inorder) {
-  if (
-    !preorder ||
-    !inorder ||
-    preorder.length === 0 ||
-    inorder.length === 0 ||
-    preorder.length !== inorder.length
-  )
-    return null
-
-  let rootVal = preorder.shift()
-  let root = new TreeNode(rootVal)
-  let inPivot = inorder.indexOf(rootVal)
-  let leftInorder = inorder.slice(0, inPivot)
-  let rightInorder = inorder.slice(inPivot + 1)
-
-  root.left = buildTree(preorder.slice(0, leftInorder.length), leftInorder)
-  root.right = buildTree(preorder.slice(leftInorder.length), rightInorder)
-
-  return root
+  let node = root
+  while (node) {
+    if (node.left) {
+      let next = node.left
+      let predecessor = next
+      while (predecessor.right) {
+        predecessor = predecessor.right
+      }
+      predecessor.right = node.right
+      node.left = null
+      node.right = next
+    }
+    node = node.right
+  }
 }
