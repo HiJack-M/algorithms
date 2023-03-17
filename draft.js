69d4dbfb8c0bfc8609efe68d7a0a182087d2976a
HiJack <hijack.sherlock@live.com>
2023-03-17 15:22:04 +0800

    refactor(leetcode review): 152. Maximum Product Subarray

--- draft.js
+++ draft.js
@@ -1,111 +1,29 @@
-// 146. LRU Cache
+// 152. Maximum Product Subarray
 
-// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
 
-// Implement the LRUCache class:
-
-// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-
-// int get(int key) Return the value of the key if the key exists, otherwise return -1.
-
-// void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
-
-// The functions get and put must each run in O(1) average time complexity.
+// The test cases are generated so that the answer will fit in a 32-bit integer.
 
 /**
- * @param {number} capacity
- */
-var LRUCache = function (capacity) {
-  this.limit = capacity
-  this.keyNodeMap = new Map()
-  this.linkedList = new DoubleLinkedList()
-}
-
-/**
- * @param {number} key
+ * @param {number[]} nums
  * @return {number}
  */
-LRUCache.prototype.get = function (key) {
-  if (!this.keyNodeMap.has(key)) return -1
-  let node = this.keyNodeMap.get(key)
-  this.linkedList.moveNodeToTail(node)
-  return node.value
+var maxProduct = function (nums) {
+  if (!nums || nums.length === 0) return 0
+
+  let ans = nums[0]
+  let min = nums[0] // 以每个点结尾的最小积（这样后面就可以延续了）
+  let max = nums[0] // 以每个点结尾的最大积
+  for (let i = 1; i < nums.length; i++) {
+    let curMin = Math.min(nums[i], Math.min(min * nums[i], max * nums[i]))
+    let curMax = Math.max(nums[i], Math.max(min * nums[i], max * nums[i]))
+    min = curMin
+    max = curMax
+    ans = Math.max(ans, max)
+  }
+
+  return ans
 }
 
-/**
- * @param {number} key
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function (key, value) {
-  if (!this.keyNodeMap.has(key)) {
-    let node = new Node(key, value)
-    this.keyNodeMap.set(key, node)
-    this.linkedList.addNode(node)
-    if (this.keyNodeMap.size > this.limit) {
-      let delNode = this.linkedList.removeHead()
-      this.keyNodeMap.delete(delNode.key)
-    }
-  } else {
-    let node = this.keyNodeMap.get(key)
-    node.value = value
-    this.linkedList.moveNodeToTail(node)
-  }
-}
-
-class Node {
-  constructor(key, value) {
-    this.key = key
-    this.value = value
-    this.next = null
-    this.prev = null
-  }
-}
-
-class DoubleLinkedList {
-  constructor() {
-    this.head = null
-    this.tail = null
-  }
-
-  addNode(node) {
-    if (node == null) return
-    if (this.head == null) {
-      this.head = node
-      this.tail = node
-    } else {
-      this.tail.next = node
-      node.prev = this.tail
-      this.tail = node
-    }
-  }
-
-  removeHead() {
-    if (this.head == null) return null
-    let node = this.head
-    if (this.head == this.tail) {
-      this.head = null
-      this.tail = null
-    } else {
-      this.head = this.head.next
-      this.head.prev = null
-      node.next = null
-    }
-    return node
-  }
-
-  moveNodeToTail(node) {
-    if (node == this.tail) return
-    if (node == this.head) {
-      this.head = node.next
-      this.head.prev = null
-    } else {
-      node.next.prev = node.prev
-      node.prev.next = node.next
-    }
-    this.tail.next = node
-    node.prev = this.tail
-    this.tail = node
-    node.next = null
-  }
-}
+const nums1 = [2, 3, -2, 4]
+console.log(maxProduct(nums1))
--- problems/leetcode/medium_0152_maxProduct.js
+++ problems/leetcode/medium_0152_maxProduct.js
@@ -8,8 +8,8 @@ var maxProduct = function (nums) {
   if (!nums || nums.length == 0) return 0
 
   let ans = nums[0]
-  let min = nums[0]
-  let max = nums[0]
+  let min = nums[0] // 以每个点结尾的最小积（这样后面就可以延续了）
+  let max = nums[0] // 以每个点结尾的最大积
 
   // 以 i 结尾的最小积、最大积
   for (let i = 1; i < nums.length; i++) {
