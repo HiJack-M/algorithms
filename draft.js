bef90d1f60ab117b95a957c69a0890a4ea775fbe
HiJack <hijack.sherlock@live.com>
2023-03-23 12:51:07 +0800

    refactor(leetcode review): 287. Find the Duplicate Number

--- draft.js
+++ draft.js
@@ -1,56 +1,34 @@
-// 240. Search a 2D Matrix II
+// 287. Find the Duplicate Number
 
-// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
-// Integers in each row are sorted in ascending from left to right.
-// Integers in each column are sorted in ascending from top to bottom.
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
 
 /**
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var searchMatrix = function (matrix, target) {
-  if (!matrix || matrix.length === 0 || matrix[0].length === 0 || !target) return false
+var findDuplicate = function (nums) {
+  if (!nums || nums.length < 2) return null
 
-  let m = matrix.length
-  let n = matrix[0].length
+  let fast = nums[nums[0]]
+  let slow = nums[0]
 
-  // 从右上角开始搜索
-  // 可以使其中一维处于最小【x】，另一维处于最大【y】，方便缩小范围，只能往左或者下方缩小
-  // 每次都能排除某个点所在行或者列
-  let x = 0
-  let y = n - 1
-  while (x < m && y >= 0) {
-    if (matrix[x][y] === target) return true
-    if (matrix[x][y] < target) {
-      // 当前值小于目标，由于都是升序，要往更大看，【y】的一维已经是最大了，所以【x】维度加大，x++
-      x++
-    } else {
-      // 当前值大于目标，由于都是升序，要往更小看，【x】的一维已经是最小了，所以【y】维度减小，y--
-      y--
-    }
+  while (fast < nums.length) {
+    if (fast === slow) break
+    fast = nums[nums[fast]]
+    slow = nums[slow]
   }
 
-  return false
+  fast = 0
+  while (fast !== slow) {
+    fast = nums[fast]
+    slow = nums[slow]
+  }
+
+  return fast
 }
-
-const matrix1 = [
-  [1, 4, 7, 11, 15],
-  [2, 5, 8, 12, 19],
-  [3, 6, 9, 16, 22],
-  [10, 13, 14, 17, 24],
-  [18, 21, 23, 26, 30],
-]
-let target1 = 5
-console.log(searchMatrix(matrix1, target1))
-
-const matrix2 = [
-  [1, 4, 7, 11, 15],
-  [2, 5, 8, 12, 19],
-  [3, 6, 9, 16, 22],
-  [10, 13, 14, 17, 24],
-  [18, 21, 23, 26, 30],
-]
-let target2 = 20
-console.log(searchMatrix(matrix2, target2))
+const nums1 = [1, 2, 3, 4, 2]
+console.log(findDuplicate(nums1))
